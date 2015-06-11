25e54285585aa13ce22b8382e3d214d94f60c847
Paul Intharathut <jedi_lynx@hotmail.com>
2015-06-10 22:21:51 -0700

    Animations, Audio, and Custom Table View Cells

---
+++ Table/TrackCell.swift
@@ -0,0 +1,17 @@
+//
+//  TrackCell.swift
+//  Table
+//
+//  Created by Paul Intharathut on 6/10/15.
+//  Copyright (c) 2015 Paul Intharathut. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+    
+    @IBOutlet weak var playIcon: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
